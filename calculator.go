d9a89fe16c77380a4e07ee686913501e0ee5a135
RifqiF27 <rifqifadluloh27@gmail.com>
2024-10-21 11:16:57 +0700

    test

---
+++ calculator.go
@@ -0,0 +1,18 @@
+package calculator
+
+func Penjumlahan(a, b int) int {
+	result := a + b
+	return result
+}
+func Perkalian(a, b int) int {
+	result := a * b
+	return result
+}
+func Pembagian(a, b int) float64 {
+	result := float64(a) / float64(b)
+	return result
+}
+func Pengurangan(a, b int) int {
+	result := a - b
+	return result
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RifqiF27/package-calculator
+
+go 1.23.2
